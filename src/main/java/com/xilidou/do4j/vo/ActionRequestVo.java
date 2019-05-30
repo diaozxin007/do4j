05a863be2d830ad61c6fe284e194e1b416273c22
diaozxin007 <diaozxin@gmial.com>
2019-05-30 13:54:15 +0800

    接入 travis

--- src/main/java/com/xilidou/do4j/vo/ActionRequestVo.java
+++ src/main/java/com/xilidou/do4j/vo/ActionRequestVo.java
@@ -1,21 +1,32 @@
 package com.xilidou.do4j.vo;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.ToString;
 
 import java.util.List;
 
 
+/**
+ * @author zhengxin
+ */
 @Data
 @ToString
+@ApiModel()
 public class ActionRequestVo {
+
+	private long id;
 
 	private String title;
 
-	private String describtion;
-
-	private Long objectId;
+	private String description;
 
 	private List<Long> tagIds;
+
+	private long objectId;
+
+	private int status;
+
+	private boolean marked;
 
 }
